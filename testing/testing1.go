74a67a0841176cbf22891ef90689625260b2e1df
ImperialEmmy <oluwaseunadeeko124@gmail.com>
2026-09-24 16:15:19 +0100

    refactor main function and add division logic

--- testing/testing1.go
+++ testing/testing1.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	const myName string = "victor"
-	fmt.Println("my name is " + myName)
+// import "fmt"
 
-	var myAge int16 = 23
-	fmt.Println(myAge)
+// func main() {
+// 	const myName string = "victor"
+// 	fmt.Println("my name is " + myName)
+
+// 	var myAge int16 = 23
+// 	fmt.Println(myAge)
+// }
+
+func division(top int, bottom int) (int, int) {
+	var result = top / bottom
+	var remainder = top % bottom
+	return result, remainder
+}
+
+func main() {
+	var top int = 15
+	var bottom int = 2
+	var result, remainder int = division(top, bottom)
+
+	fmt.Printf("The result of the division is %v remainder %v", result, remainder)
 }
