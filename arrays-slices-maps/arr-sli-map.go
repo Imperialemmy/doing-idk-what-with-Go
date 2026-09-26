786df58c415422bbb95fc8ce7d335bdff8f3fa0d
ImperialEmmy <oluwaseunadeeko124@gmail.com>
2026-09-26 09:20:27 +0100

    learning arrays, maps and slices (i get headache mehn)

---
+++ arrays-slices-maps/arr-sli-map.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	ages := make(map[string]int)
+// 	ages["John"] = 25
+// 	ages["Mary"] = 30
+// 	for key, value := range ages {
+// 		fmt.Println(key, value)
+// 	}
+// }
+
+func displayages(name string, age int) {
+	ages := map[string]int{
+		name: age,
+	}
+	for key, value := range ages {
+		fmt.Println(key, value)
+	}
+}
+
+func main() {
+	// displayages("seun", 23)
+	// displayages("seun girl", 18)
+	// displayages("daddy", 62)
+	// displayages("mummy", 57)
+	// displayages("ay", 26)
+	displayages("johnny", 23)
+	displayages("kiing", 25)
+	fmt.Println("omo una don old o")
+}
---
+++ arrays-slices-maps/go.mod
@@ -0,0 +1,3 @@
+module gamegame.com/arr-sli-map
+
+go 1.27.1
